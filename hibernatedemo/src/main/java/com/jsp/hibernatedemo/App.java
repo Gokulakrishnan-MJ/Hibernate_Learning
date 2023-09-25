be31b08939d815b06c998c7ee01bc037cd7e6d09
GOKULAKRISHNAN MURUGAN <gokulakrishnan.m3009@gmail.com>
2023-09-25 19:44:31 +0530

    25-sep commit

--- hibernatedemo/src/main/java/com/jsp/hibernatedemo/App.java
+++ hibernatedemo/src/main/java/com/jsp/hibernatedemo/App.java
@@ -1,5 +1,12 @@
 package com.jsp.hibernatedemo;
 
+import com.jsp.hibernatedemo.dto.Student;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //EntityManagerFactory createEntityManagerFactory = Persistence.createEntityManagerFactory("development");
+        saveStudentData();
+    }
+    public static void saveStudentData() {
+        
+        Student student1 =new Student();
+        student1.setStudentID(001);
+        student1.setStudentName("Gokul");
+        student1.setStudentEmail("suren7219@gmail.com");
+        student1.setAddress("chennai");
+        
+        // jpa code here
+        
+        EntityManagerFactory EntityManagerFactory = Persistence.createEntityManagerFactory("development");
+        EntityManager manager = EntityManagerFactory.createEntityManager();
+        EntityTransaction entitytTransaction = manager.getTransaction();
+        
+        entitytTransaction.begin();
+        manager.persist(student1);
+        entitytTransaction.commit();
+        
     }
 }
+ 
--- hibernatedemo/src/main/java/com/jsp/hibernatedemo/dto/Student.java
+++ hibernatedemo/src/main/java/com/jsp/hibernatedemo/dto/Student.java
@@ -1,5 +1,46 @@
 package com.jsp.hibernatedemo.dto;
 
-public class Student {
+import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class Student implements Serializable{
+	@Id
+	private int studentID;
+	private String studentName;
+	private String studentEmail;
+	private String address;
+	
+	public Student() {
+		super();
+	}
+	
+	public int getStudentID() {
+		return studentID;
+	}
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentEmail() {
+		return studentEmail;
+	}
+	public void setStudentEmail(String studentEmail) {
+		this.studentEmail = studentEmail;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	
+	
 }
