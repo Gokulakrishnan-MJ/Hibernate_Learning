227e5fc47953c29494c4132a750749e9faff136c
GOKULAKRISHNAN MURUGAN <gokulakrishnan.m3009@gmail.com>
2023-10-06 16:26:40 +0530

    JPQL Project created

---
+++ JPQL-Example/src/main/java/com/jsp/JPQL_Example/App.java
@@ -0,0 +1,13 @@
+package com.jsp.JPQL_Example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
